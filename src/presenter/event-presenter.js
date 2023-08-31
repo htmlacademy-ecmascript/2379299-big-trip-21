10ec6d334b2edddff1d222562d8c1773839012d0
Moon Studico <moonstudico@gmail.com>
2023-08-31 11:11:52 +0200

    Добавит возможность переиспользования

--- src/presenter/event-presenter.js
+++ src/presenter/event-presenter.js
@@ -1,6 +1,6 @@
 import EventItemView from '../view/event-item-view';
 import ListFormView from '../view/event-item-form-view.js';
-import {render, replace} from '../framework/render.js';
+import {render, replace, remove} from '../framework/render.js';
 
 
 export default class EventPresenter {
@@ -16,6 +16,8 @@ export default class EventPresenter {
 
   init(point){
     this.#point = point;
+    const prevPointItem = this.#pointItem;
+    const prevPointForm = this.#pointForm;
 
     this.#pointItem = new EventItemView({point: this.#point,
       onClick: this.#handleOnClick
@@ -26,7 +28,26 @@ export default class EventPresenter {
       onFormSubmit: this.#handleOnFormSubmit
     });
 
-    render(this.#pointItem, this.#containerForEvent);
+    if (prevPointItem === null || prevPointForm === null){
+      render(this.#pointItem, this.#containerForEvent);
+      return;
+    }
+    if (this.#containerForEvent.contains(prevPointItem.element)) {
+      replace(this.#pointItem, prevPointItem);
+    }
+
+    if (this.#containerForEvent.contains(prevPointForm.element)) {
+      replace(this.#pointForm, prevPointForm);
+    }
+
+    remove(prevPointItem);
+    remove(prevPointForm);
+
+  }
+
+  destroy() {
+    remove(this.#pointItem);
+    remove(this.#pointForm);
   }
 
   #escKeyDownHandler = (evt) => {
@@ -55,3 +76,4 @@ export default class EventPresenter {
     document.removeEventListener('keydown', this.escKeyDownHandler);
   }
 }
+
