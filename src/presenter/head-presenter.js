9d6efca9a25f5339c0d3eb8eebc522f253940a79
Moon Studico <moonstudico@gmail.com>
2023-10-07 10:28:14 +0200

    добавление городов и стоимости

--- src/presenter/head-presenter.js
+++ src/presenter/head-presenter.js
@@ -6,7 +6,7 @@ export default class HeadPresenter {
   #siteHeadContainer = null;
   #filterModel = null;
   #pointModel = null;
-  #listInfo = new ListEventInfoView();
+  #listInfo = null;
   #listFilter = null;
   #addButton = null;
 
@@ -18,6 +18,9 @@ export default class HeadPresenter {
     this.#siteHeadContainer = siteHeadContainer;
     this.#filterModel = filterModel;
     this.#pointModel = pointModel;
+    this.#listInfo = new ListEventInfoView({
+      pointModel: pointModel
+    });
 
     this.#pointModel.addObserver(this.#handleModelEvent);
     this.#addButton = document.querySelector('.trip-main__event-add-btn');
@@ -25,6 +28,7 @@ export default class HeadPresenter {
   }
 
   init(){
+
     this.#listFilter = new ListFilterView({
       onClickTypeFilter: this.#handleClickTypeFilter,
       pointModel: this.#pointModel,
--- src/view/list-event-info-view.js
+++ src/view/list-event-info-view.js
@@ -1,21 +1,75 @@
 import AbstractView from '../framework/view/abstract-stateful-view.js';
 
-function createEventInfoTemplate() {
+function createEventInfoTemplate({sum, cities}) {
+  const citiesString = cities.join(' &mdash; ');
+
   return (
     ` <section class="trip-main__trip-info  trip-info">
         <div class="trip-info__main">
-          <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+          <h1 class="trip-info__title">${citiesString}</h1>
           <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
         </div>
 
         <p class="trip-info__cost">
-          Total: &euro;&nbsp;<span class="trip-info__cost-value">1230</span>
+          Total: &euro;&nbsp;<span class="trip-info__cost-value">${sum}</span>
         </p>
       </section>`
   );
 }
 export default class ListEventInfoView extends AbstractView{
+  #pointModel = null;
+
+  constructor({pointModel}) {
+    super();
+    this.#pointModel = pointModel;
+    this.#pointModel.addObserver(this.#handleModelEvent);
+    this._setState({
+      sum: 0,
+      cities: ['Amsterdam','Chamonix','Geneva']
+    });
+
+  }
+
+  #calculateSum(points) {
+    return points?.reduce((accumulator, current) => {
+      const pointsPrice = accumulator + current.price;
+      const sumOfOffers = current.offers?.reduce((offerSum, offer) => offerSum + offer.price, 0);
+      return pointsPrice + sumOfOffers;
+    }, 0);
+  }
+
+  #calculateCities(points){
+    const counter = points.reduce((sum) => sum + 1, 1);
+
+    if (counter === 0){
+      return [];
+    }
+
+    if (counter > 3){
+      return [
+        points[0]?.destination?.name,
+        '...',
+        points[counter - 2]?.destination?.name
+      ];
+    }
+
+    return points.map((current) => current?.destination?.name);
+  }
+
+  #handleModelEvent = () => {
+    const points = this.#pointModel.points;
+    this.updateElement({
+      sum: this.#calculateSum(points),
+      cities: this.#calculateCities(points)
+    });
+  };
+
+  _restoreHandlers(){ }
+
   get template() {
-    return createEventInfoTemplate();
+    return createEventInfoTemplate({
+      sum: this._state?.sum || 0,
+      cities: this._state?.cities || []
+    });
   }
 }
